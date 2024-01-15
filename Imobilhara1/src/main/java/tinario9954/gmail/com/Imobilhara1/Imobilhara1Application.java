64d4ee90a807774cb7312d264041a3c2034223cc
Matias G Adelina <tinario9954@gmail.com>
2024-01-15 18:06:08 +0100

    primera fase
    15/01/2023

---
+++ Imobilhara1/src/main/java/tinario9954/gmail/com/Imobilhara1/Imobilhara1Application.java
@@ -0,0 +1,13 @@
+package tinario9954.gmail.com.Imobilhara1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Imobilhara1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Imobilhara1Application.class, args);
+	}
+
+}
---
+++ Imobilhara1/src/test/java/tinario9954/gmail/com/Imobilhara1/Imobilhara1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tinario9954.gmail.com.Imobilhara1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Imobilhara1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
